b0a493f044dbcc5390185ddafcf2f7491d242d41
tuongnhisummer <165285621+tuongnhisummer@users.noreply.github.com>
2024-04-10 20:39:50 +0700

    Update Day11.sql

--- Day11.sql
+++ Day11.sql
@@ -1,4 +1,3 @@
-
 --1
 Select Country.continent, round(avg(city.population),0)
 From City
@@ -6,6 +5,11 @@ Join Country
 On City.countrycode = Country.Code
 Group by Country.continent
 order by round(avg(city.population),0)
+--2
+SELECT cast(sum(case when t.signup_action='Confirmed' then 1 else 0 end) as real)/count(e.email_id) as activation_rate
+FROM emails e
+RIGHT JOIN texts t
+on e.email_id=t.email_id
 --4
 SELECT c.customer_id
 FROM customer_contracts c
